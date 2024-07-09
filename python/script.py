3473b2feaffb823583a89b9cc5145aef02190f64
DHE <ydh.postkasten@yahoo.com>
2024-07-09 14:38:54 +0200

    dev chat id II

--- python/script.py
+++ python/script.py
@@ -5,7 +5,7 @@ API_TOKEN = "7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM"
 
 def send_message(chat_id, message):
     url = f"https://api.telegram.org/bot{7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
+    data = {"chat_id": 1501637981, "text": message}
     requests.post(url, json=data)
 
 def main():

de51428f38a066aed0e7b355f809d93abdce4dcb
DHE <ydh.postkasten@yahoo.com>
2024-07-09 13:56:59 +0200

    dev

---
+++ JSON/script.js
@@ -0,0 +1,10 @@
+const token = '7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM' ;
+
+// const url = "https://api.telegram.org/bot" + token + '/sendMessage' ;
+
+function scheduleMessage(){
+
+     let text = document.getElementById('textMessage').value;
+     const fullUrl = url + '?chat_id=' + chadId + "&text=" + Text;
+
+}

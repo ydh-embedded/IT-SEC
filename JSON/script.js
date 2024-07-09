93a5280f910a101b941b6cf9b593ca771d63a7a9
DHE <ydh.postkasten@yahoo.com>
2024-07-09 14:32:36 +0200

    dev

--- JSON/script.js
+++ JSON/script.js
@@ -5,6 +5,10 @@ const token = '7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM' ;
 function scheduleMessage(){
 
      let text = document.getElementById('textMessage').value;
+     let chatID =  123456798 ;    
      const fullUrl = url + '?chat_id=' + chadId + "&text=" + Text;
-
-}
+     
+     setTimeout(function(){
+          fetch (fullUrl, {method: 'POST'});
+     }, 5000);
+}  
--- python/script.py
+++ python/script.py
@@ -1,9 +1,10 @@
 import requests
 
-API_TOKEN = "YOUR_API_TOKEN"
+# API_TOKEN = "YOUR_API_TOKEN"
+API_TOKEN = "7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM"
 
 def send_message(chat_id, message):
-    url = f"https://api.telegram.org/bot{API_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     requests.post(url, json=data)
 

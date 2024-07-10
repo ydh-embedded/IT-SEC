001a39e38656d0cb3cd4a916ce2b87bc2d2c2347
DHE <ydh.postkasten@yahoo.com>
2024-07-10 09:34:30 +0200

    dev

--- JSON/script.js
+++ JSON/script.js
@@ -1,14 +1,14 @@
-const token = '7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM' ;
-
+// const token = '7493318142:AAGp9nDF9QD-VAFdZIxMJe5uzXHoiahdZoM' ;
+//
 // const url = "https://api.telegram.org/bot" + token + '/sendMessage' ;
-
-function scheduleMessage(){
-
-     let text = document.getElementById('textMessage').value;
-     let chatID =  123456798 ;    
-     const fullUrl = url + '?chat_id=' + chadId + "&text=" + Text;
-     
-     setTimeout(function(){
-          fetch (fullUrl, {method: 'POST'});
-     }, 5000);
-}  
+// 
+// function scheduleMessage(){
+// 
+//      let text = document.getElementById('textMessage').value;
+//      let chatID =  123456798 ;    
+//      const fullUrl = url + '?chat_id=' + chadId + "&text=" + Text;
+//      
+//      setTimeout(function(){
+//           fetch (fullUrl, {method: 'POST'});
+//      }, 5000);
+// }  
